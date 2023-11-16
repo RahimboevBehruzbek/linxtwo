9df0f59f62f7a13129d89e5f0a4e059b01581fc9
Rahimboeyev Behruz <Raximboevbexruz@gmail.com>
2023-11-16 21:23:06 +0500

    first commit

---
+++ src/pages/InstagramAds/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import InstagramAdsHeader from './InstagramAdsHeader'
+import InstagramAdsQuestions from './InstagramAdsQuestions'
+import InstagramAdsWork from './InstagramAdsWork'
+
+const IndexINS = () => {
+  return (
+    <>
+      <InstagramAdsHeader />
+      <InstagramAdsWork />
+      <InstagramAdsQuestions />
+    </>
+  )
+}
+
+export default IndexINS
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../layout/Main/Main'
+
+
+const Home = () => {
+  return (
+    <div className='main'>
+      <Main/>
+    </div>
+  )
+}
+
+export default Home
